18ca0dddab73efd4337dc4e7c0ec922051b32f67
Antonino <antonino.mac93@gmail.com>
2016-01-08 12:08:27 +0100

    riadattato il codice per dei suggerimenti della prof

--- public_html/javascript/control.js
+++ public_html/javascript/control.js
@@ -21,7 +21,7 @@
 //id="logErr" dentro il tag p del messaggio di errore
 
 //pass_recovery.html
-//onsubmit="return recoveryContol()" dentro tag form
+//onsubmit="return recoveryControl()" dentro tag form
 //name="pass_recovery" dentro tag form di login
 //id="recErr" dentro il tag p del messaggio di errore di recupero password
 
@@ -36,6 +36,10 @@
 //name="profileChange" dentro tag form
 //id="name","surname","age","email" aggiunti a tutti i tag input della form
 //tutti i value=" " dentro i tag della form sono stati sostituiti con value=""; per poter controllare se il campo era stato lasciato vuoto dall'utente
+
+
+//appunto di codice: document.getElementById("recErr").style.color='red';
+
 function profileChangeControl(){
     var x=document.forms["profileChange"]["name"].value;
     if (x == null || x == "") {
@@ -86,99 +90,120 @@ function profileChangeControl(){
     }
 }
 
-function registrationControl(){ //controllare validita' della data nascita
-    var x=document.forms["registration"]["reg_name"].value;
-    if (x == null || x == "") {
-        document.getElementById("regErr").innerHTML = "*campo nome vuoto";
-        return false;
-    }
-    x=document.forms["registration"]["reg_surname"].value;
-    if (x == null || x == "") {
-        document.getElementById("regErr").innerHTML = "*campo cognome vuoto";
-        return false;
-    }
-    //caso data nascita vuoto->da sostituire
-    x=document.forms["registration"]["reg_eta"].value;
-    if (x == null || x == "") {
-        document.getElementById("regErr").innerHTML = "*campo data nascita vuoto";
-        return false;
-    }
-    x=document.forms["registration"]["reg_username"].value;
-    if (x == null || x == "") {
-        document.getElementById("regErr").innerHTML = "*campo username vuoto";
-        return false;
-    }
-    x=document.forms["registration"]["reg_email"].value;
-    if (x == null || x == "") {
-        document.getElementById("regErr").innerHTML = "*campo Email vuoto";
-        return false;
-    }
-    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-    if(!re.test(x)){
-      document.getElementById("regErr").innerHTML = "*Email non valida";
-      return false;
-    }
-    x=document.forms["registration"]["reg_pass"].value;
-    if (x == null || x == "") {
-        document.getElementById("regErr").innerHTML = "*campo password vuoto";
-        return false;
-    }    
-    if( x.length<'8'){
-        document.getElementById("regErr").innerHTML = "*campo password deve contenere almeno 8 caratteri";
-        return false;
-    }
-    var y=document.forms["registration"]["reg_re_pass"].value;
-    if (y == null || y == "") {
-        document.getElementById("regErr").innerHTML = "*campo ripeti la password vuoto";
-        return false;
-    }
-    if( y.length<'8'){
-        document.getElementById("regErr").innerHTML = "*campo ripeti la password deve contenere almeno 8 caratteri";
-        return false;
-    }
-    if(x!=y){
-        document.getElementById("regErr").innerHTML = "*i campi pssword e ripeti password devono coincidere";
-        return false;
-    }
+
+//registrationControl
+function registrationControl(){
+    var risultato=checkName("reg_name", "regErr");
+    risultato=risultato && checkSurname("reg_surname", "regErr");    
+    risultato=risultato && checkUser("reg_username", "regErr");
+    risultato=risultato && checkEmail("reg_email", "regErr");
+    risultato=risultato && checkPassword("reg_pass", "regErr");
+    risultato=risultato && checkConfirmPassword("reg_pass", "reg_re_pass" ,"regErr");
+    
+    //scrollo la pagina poichè la form è troppo lunga e c'è il rischio che l'utente non veda il suggerimento dell'errore
+    var w = window.screen.width;
+    var h = window.screen.height;
+    window.scrollTo(3/5*w, 5/12*h);
+    return risultato;
 }
 
-function recoveryContol(){
-    var x=document.forms["pass_recovery"]["recover_name"].value;
-    if (x == null || x == "") {
-        document.getElementById("recErr").innerHTML = "*campo nome vuoto";
-        return false;
-    }
-    x=document.forms["pass_recovery"]["recover_surname"].value;
-    if (x == null || x == "") {
-        document.getElementById("recErr").innerHTML = "*campo cognome vuoto";
-        return false;
-    }
-    x=document.forms["pass_recovery"]["recover_email"].value;
-    if (x == null || x == "") {
-        document.getElementById("recErr").style.color='red';
-        document.getElementById("recErr").innerHTML = "*campo Email vuoto";
-        return false;
-    }
-    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-    if(!re.test(x)){
-      document.getElementById("recErr").innerHTML = "*Email non valida";
-      return false;
-    }
+
+//recoveryControl
+function recoveryControl(){
+    var risultato=checkName("recover_name", "recErr");
+    risultato=risultato && checkSurname("recover_surname", "recErr");
+    risultato=risultato && checkEmail("recover_email", "recErr");
+    return risultato;
 }
 
+
+//loginControl
 function loginControl(){
-    var x=document.forms["formLogin"]["login_user"].value;
-    if (x == null || x == "") {
-        document.getElementById("logErr").innerHTML = "*campo username vuoto";
-        return false;
+    var risultato=checkUser("login_user","logErr");
+    risultato=risultato && checkPassword("login_password","logErr");
+    return risultato;
+}
+
+
+
+//FUNZIONI DI CONTROLLO
+function checkUser(par, err){
+    var tag= document.getElementById(par);
+    var b=true;
+    if (tag.value == null || tag.value == "") {
+        document.getElementById(err).innerHTML = "*campo username vuoto";
+        b=false;
     }
-    x=document.forms["formLogin"]["login_password"].value;
-    if (x == null || x == "") {
-        document.getElementById("logErr").innerHTML = "*campo password vuoto";
-        return false;
+    return b;
+}
+
+function checkPassword(par, err){
+    var tag= document.getElementById(par);
+    var b=true;
+    if (tag.value == null || tag.value == "") {
+        document.getElementById(err).innerHTML = "*campo password vuoto";
+        b=false;
     }
-    if( x.length<'8'){
-        document.getElementById("logErr").innerHTML = "*campo password deve contenere almeno 8 caratteri";
-        return false;
+    if(b && tag.value.length<'8'){
+        document.getElementById(err).innerHTML = "*campo password deve contenere almeno 8 caratteri";
+        b=false;
     }
+    return b;
+}
+
+function checkConfirmPassword(pass, conf_pass, err){
+    var tag= document.getElementById(pass);
+    var tag2= document.getElementById(conf_pass);
+    var b=true;
+    if (tag2.value == null || tag2.value == "") {
+        document.getElementById(err).innerHTML = "*campo Ripeti la Password vuoto";
+        b=false;
+    }
+    if(b && tag2.value.length<'8'){
+        document.getElementById(err).innerHTML = "*campo Ripeti la Password deve contenere almeno 8 caratteri";
+        b=false;
+    }
+    if(b && tag.value!=tag2.value){
+        document.getElementById(err).innerHTML = "*campo Password deve coincidere con il campo Ripeti la Password";
+        b=false;
+    }
+
+
+    return b;
+}
+
+
+function checkName(par, err){
+    var tag= document.getElementById(par);
+    var b=true;
+    if (tag.value == null || tag.value == "") {
+        document.getElementById(err).innerHTML = "*campo Nome vuoto";
+        b=false;
+    }
+    return b;
+}
+
+function checkSurname(par, err){
+    var tag= document.getElementById(par);
+    var b=true;
+    if (tag.value == null || tag.value == "") {
+        document.getElementById(err).innerHTML = "*campo Cognome vuoto";
+        b=false;
+    }
+    return b;
+}
+
+function checkEmail(par, err){
+    var tag= document.getElementById(par);
+    var b=true;
+    if (tag.value == null || tag.value == "") {
+        document.getElementById(err).innerHTML = "*campo Email vuoto";
+        b=false;
+    }
+    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    if(b && !re.test(tag.value)){
+        document.getElementById(err).innerHTML = "*Email non valida";
+        b=false;
+    }
+    return b;
 }
