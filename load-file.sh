554e36c4a2839044b4da8a66e2a81f30772ccd3a
drigoni <davider1994@gmail.com>
2016-01-28 20:22:09 +0100

    Sistemato redirect logo

--- load-file.sh
+++ load-file.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-scp  -P 30022   -r -p  cgi-bin/* amacri@localhost:tecweb/cgi-bin/
-scp  -P 30022   -r -p  public_html/* amacri@localhost:tecweb/public_html/
-scp  -P 30022   -r -p data/* amacri@localhost:tecweb/data/
+scp  -P 30022   -r -p  cgi-bin/* drigoni@localhost:tecweb/cgi-bin/
+scp  -P 30022   -r -p  public_html/* drigoni@localhost:tecweb/public_html/
+scp  -P 30022   -r -p data/* drigoni@localhost:tecweb/data/
+
