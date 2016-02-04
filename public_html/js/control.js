2c4835a758e72af49696fe6ea9440f8fc911f3da
Antonino <antonino.mac93@gmail.com>
2016-02-04 11:45:18 +0100

    404 not found

--- public_html/js/control.js
+++ public_html/js/control.js
@@ -1,56 +1,4 @@
-//Aggiunto controllo javascript in tutte le pagine
-
-//COSA NON HO FATTO:controllo della correttezza della data di nascita inserita dentro la pagina registration.html
-//NOTA BENE: Per la Data preferirei che venissero inseriti 3 campi input per giorno,mese ed anno er evitare problemi di forma es: dd.mm.yyyy oppure mm.dd.yyyy, vincolando l'utente ad un solo modo valido per l'inserimento dei dati (dd mm yyyy)
-//NOTA BENE2: controllare che il regex usato sia corretto per tutte le possibili e-mail(intanto controllare solo che le email che abbiamo siano tutte accettate) 
-
-//COSA HO FATTO
-//AZIONI
-//1)controllo campi non vuoti
-//2)controllo campo password almeno di 8 caratteri
-//3)controllo campo password=reinserisci la password
-//4)controllo email valida tramite regex
-
-//AGGIUNTE DI CODICE (nelle pagine HTML DI DAVIDE E JACK)
-//IN TUTTI I FILE DI DAVIDE
-//<script type="text/javascript" src="../javascript/control.js"></script> nell'head
-
-//home.html, pass_recovery.html e registration.html
-//onsubmit="return loginControl()" dentro tag form di login
-//onkeypress="return loginControl()" dentro tag form di login
-//name="formLogin" dentro tag form di login
-//id="logErr" dentro il tag p del messaggio di errore
-
-//pass_recovery.html
-//onsubmit="return recoveryControl()" dentro tag form
-//onkeypress="return recoveryControl()" dentro tag form di login
-//name="pass_recovery" dentro tag form di login
-//id="recErr" dentro il tag p del messaggio di errore di recupero password
-
-//registration.html
-//onsubmit="return registrationControl()" dentro tag form
-//onkeypress="return registrationControl()" dentro tag form di login
-//name="registration" dentro tag form di login
-//id="regErr" dentro il tag p del messaggio di errore
-
-//profile_change.html
-//<script type="text/javascript" src="../../javascript/control.js"></script> nell'head
-//onsubmit="return profileChangeControl()" dentro tag form
-//name="profileChange" dentro tag form
-//id="name","surname","age","email" aggiunti a tutti i tag input della form
-//tutti i value=" " dentro i tag della form sono stati sostituiti con value=""; per poter controllare se il campo era stato lasciato vuoto dall'utente
-
-//profile_change.cgi
-//aggiunto id="username" al tag input
-//onkeypress="return profileChangeControl()" dentro tag form di login
-
-//appunto di codice: document.getElementById("recErr").style.color='red';
-
-//addInsertion.cgi
-//aggiunto id="addDescrizione"
-//onsubmit="return addInsertionControl()" 
-//onkeypress="return addInsertionControl()"
-
+//Funzioni Javascript
 //addInsertion
 function addInsertionControl(){
     var risultato=checkTitolo("addTitolo","cont_msg");
@@ -380,15 +328,6 @@ function checkDescrizione(par, err){
         b=false;
     }
     return b;
-}
-
-function alertOnRmProfile()
-{
-    var x = confirm("Sei sicuro di cancellare definitivamente il tuo profilo da SitesBoard?");
-      if (x)
-          return true;
-      else
-        return false;
 }
 
 function cleanField(par){ //uso nel campo input aggiungere: onfocus="cleanField('id_del_campo')"
